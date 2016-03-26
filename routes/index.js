04d08d7b734e8928bc6446d9029b2a134e07f680
Jake Macek <jmacek@Ksu.edu>
2016-03-26 00:25:30 -0500

    Set up basic UI for pages, couldn't get challange page to center though, will work on that when I have time

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home Page' });
 });
 router.get('/mockup', function(req, res, next) {
-  res.render('mockup.html', { title: 'Mockup Page' });
+  res.render('mockup', { title: 'Mockup Page' });
 });
-
+router.get('/challenge', function(req, res, next) {
+  res.render('challenge', { title: 'Challenge Page' });
+});
+router.get('/scoreboard', function(req, res, next) {
+  res.render('scoreboard', { title: 'Scoreboard page' });
+});
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login' });
+});
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Register Here' });
+});
 module.exports = router;
