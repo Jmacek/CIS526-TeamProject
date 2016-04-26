60a59e4869882504ad978c07b6086b808e695da9
Gannon Huiting <gannonhuiting@gmail.com>
2016-04-26 11:42:13 -0500

    testing does not work

---
+++ test/app_test.js
@@ -0,0 +1,68 @@
+/**
+ * Created by gannonhuiting on 4/25/16.
+ */
+process.env.NODE_ENV = 'test';
+
+var app = require('../app.js'),
+    server,
+    http = require('http'),
+    assert = require('assert'),
+    fs = require('fs'),
+    url = require('url'),
+    config = require('../config/test'),
+    host = config.host;
+
+
+before(function() {
+    // Launch the server
+    app.start();
+
+    // also, store the app globally
+    global.app = app;
+});
+
+after(function(){
+    console.log("shutting down...")
+    app.stop();
+});
+
+describe('app tests', function() {
+
+    it('app should exist', function() {
+        assert.ok(app);
+    });
+
+    //it('should be listening at ' + host, function(done){
+    //    http.get(host, function(res) {
+    //        console.log(res.statusCode)
+    //        assert.equal(res.statusCode, 404);
+    //        done();
+    //    });
+    //});
+
+    //function testStaticFile(url, path) {
+    //    it('should serve ' + url + ' from ' + path, function(done) {
+    //        var fileBody = fs.readFileSync(path);
+    //
+    //        http.get(host + url, function(res) {
+    //            assert.equal(res.statusCode, 200);
+    //            var body = "";
+    //            res.on('data', function(data) {body += data;});
+    //            res.on('end', function() {
+    //                assert.equal(fileBody, body);
+    //                done();
+    //            });
+    //            res.on('err', function(err) {done(err);});
+    //        });
+    //    });
+    //}
+    //
+    //[
+    //    ['/rides.css', './public/rides.css'],
+    //    ['/rides.js', './public/rides.js'],
+    //    ['/favicon.ico', './public/favicon.ico']
+    //].forEach(function(spec){
+    //    testStaticFile(spec[0], spec[1]);
+    //});
+
+});
