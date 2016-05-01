bd2b36a444d2f2c5d2a6f2db3e53936ba6c06a42
Gannon Huiting <gannonhuiting@gmail.com>
2016-05-01 18:58:38 -0500

    only have status codes working

--- test/app_test.js
+++ test/app_test.js
@@ -33,48 +33,29 @@ describe('app tests', function() {
         });
     });
 
-    //im tired and done with javascripts bs
-    function getElement(name, body)
+    function testStatusCode(url, statusCode)
     {
-        var p = body.split("<"+name+">");
-        var r = p[1].split("</"+name+">")[0];
-        return r;
-    }
-
-    function HTMLObject(body)
-    {
-        var output = {
-            title: getElement("title",body),
-            style: getElement("style",body),
-            script: getElement("script",body)
-        }
-        return output;
-    }
-
-    function testStaticFile(url, path) {
-        it('should serve ' + url + ' from ' + path, function(done) {
-            var fileBody = fs.readFileSync(path, {encoding: "utf-8"});
-            http.get(host + url, function(res) {
-                assert.equal(res.statusCode, 200);
-                var body = "";
-                res.on('data', function(data) {body += data;});
-                res.on('end', function() {
-                var htmlFile = HTMLObject(fileBody);
-                var htmlRes = HTMLObject(body);
-                assert.equal(htmlFile,htmlRes);
-                });
-                //res.on('err', function(err) {done(err);});
+        it('should have status code: '+statusCode, function(done) {
+            http.get(host + url,function(res){
+                //console.log("codes:",res.statusCode,statusCode);
+                assert.equal(res.statusCode,statusCode);
                 done();
             });
+
         });
     }
 
     [
-        ['/mockup', './public/mockup.html'],
-        //['/rides.js', './public/rides.js'],
-        //['/favicon.ico', './public/favicon.ico']
+        ['/challenge',200],
+        ['/scoreboard',200],
+        ['/login',200],
+        ['/logout',200],
+        ['/register',200],
+        ['/foobar',404],
+        ['/',200]
     ].forEach(function(spec){
-        testStaticFile(spec[0], spec[1]);
+        testStatusCode(spec[0],spec[1])
     });
+
 
 });
