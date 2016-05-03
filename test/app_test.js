033accbdfe444fc86f4c0e9b832adff7d0a0916d
Gannon Huiting <gannonhuiting@gmail.com>
2016-05-03 10:09:26 -0500

    its working

--- test/app_test.js
+++ test/app_test.js
@@ -33,29 +33,71 @@ describe('app tests', function() {
         });
     });
 
-    function testStatusCode(url, statusCode)
+
+
+
+    function testStatusCode(url, statusCode, title)
     {
         it('should have status code: '+statusCode, function(done) {
             http.get(host + url,function(res){
-                //console.log("codes:",res.statusCode,statusCode);
                 assert.equal(res.statusCode,statusCode);
-                done();
+                var body = "";
+                res.on('data', function(data) {body += data;});
+                res.on('end', function() {
+                    assert.equal(res.statusCode, statusCode);
+                    var resTitle = body.split("<title>")[1].split("</title>")[0];
+                    assert.equal(resTitle,title);
+                    done();
+                });
+                res.on('err', function(err){done(err);});
             });
 
         });
     }
 
+    function testAnswers(url,answerPath){
+        console.log("testAnswers is running");
+        it('should match info in + ' + answerPath, function(done) {
+            var fileBody = fs.readFileSync(answerPath);
+            var answer = JSON.parse(fileBody);
+            http.get(host + url,function(res){
+                var body = "";
+                res.on('data', function(data) {body += data;});
+                res.on('end', function() {
+                    assert.equal(res.statusCode,answer.statusCode);
+                    assert.equal(res.body);
+                    done();
+                });
+                res.on('err', function(err) {done(err);});
+            });
+        });
+    }
+
+
+
     [
-        ['/challenge',200],
-        ['/scoreboard',200],
-        ['/login',200],
-        ['/logout',200],
-        ['/register',200],
-        ['/foobar',404],
-        ['/',200]
+        ['/challenge',200,"Challenge Page"],
+        ['/scoreboard',200,"Scoreboard page"],
+        ['/login',200,"Login"],
+        ['/logout',200,""],
+        ['/register',200,"Register Here"],
+        ['/foobar',404,""],
+        ['/',200, "Home Page"]
     ].forEach(function(spec){
-        testStatusCode(spec[0],spec[1])
+        testStatusCode(spec[0],spec[1],spec[2]);
     });
+
+    //[
+    //    ['/challenge',200],
+    //    ['/scoreboard',200],
+    //    ['/login',200],
+    //    ['/logout',200],
+    //    ['/register',200],
+    //    ['/foobar',404],
+    //    ['/',200]
+    //].forEach(function(spec){
+    //    testStatusCode(spec[0],spec[1])
+    //});
 
 
 });
